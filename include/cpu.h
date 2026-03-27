3ad20036c8cd9c73fcf0aff317edea278400eba0
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-03-27 20:42:10 +0530

    add cpu_run

--- include/cpu.h
+++ include/cpu.h
@@ -8,6 +8,8 @@
 
 typedef uint16_t u16;
 typedef uint8_t u8;
+typedef int8_t i8;
+typedef int16_t i16;
 
 typedef struct cpu
 {
--- src/cpu.c
+++ src/cpu.c
@@ -46,3 +46,76 @@ void cpu_load(CPU *cpu, const char *filename)
 
     fclose(fp);
 }
+
+void cpu_run(CPU *cpu)
+{
+    while (cpu->running)
+    {
+        u16 instr = cpu->memory[cpu->pc++];
+
+        u8 opcode = (instr >> 12) & 0xF;
+        u8 ra = (instr >> 8) & 0xF;
+        u8 rb = (instr >> 4) & 0xF;
+        u8 rd = instr & 0xF;
+        u8 imm8 = instr & 0xFF;
+        u8 mode = (instr >> 11) & 0x1;
+        u16 addr = instr & 0x7FF;
+        i8 offset = (i8)((instr & 0xF) << 4) >> 4;
+
+        switch (opcode)
+        {
+        case 0x0:
+            cpu->reg[rd] = cpu->reg[ra] + cpu->reg[rb];
+            break; // add
+        case 0x1:
+            cpu->reg[rd] = cpu->reg[ra] - cpu->reg[rb];
+            break; // sub
+        case 0x2:
+            cpu->reg[rd] = ~(cpu->reg[ra] & cpu->reg[rb]);
+            break; // nand
+        case 0x3:
+            cpu->reg[rd] = cpu->reg[ra] << (cpu->reg[rb] & 0xF);
+            break; // shl
+        case 0x4:
+            cpu->reg[rd] = cpu->reg[ra] >> (cpu->reg[rb] & 0xF);
+            break; // shr
+        case 0x5:
+            cpu->reg[rb] = cpu->memory[cpu->reg[ra] + (i16)offset];
+            break; // load
+        case 0x6:
+            cpu->memory[cpu->reg[ra] + (i16)offset] = cpu->reg[rb];
+            break; // store
+        case 0x7:
+            cpu->pc = mode ? addr : cpu->reg[ra];
+            break; // jmp
+        case 0x8:
+            if ((i16)cpu->reg[ra] < (i16)cpu->reg[rb])
+                cpu->pc += offset;
+            break; // jlt
+        case 0x9:
+            if (cpu->reg[ra] == cpu->reg[rb])
+                cpu->pc += offset;
+            break; // jeq
+        case 0xA:  // call
+            cpu->reg[0xF] = cpu->pc;
+            cpu->pc = mode ? addr : cpu->reg[ra];
+            break;
+        case 0xC:
+            cpu->reg[rd] = cpu->reg[ra];
+            break; // mov
+        case 0xD:
+            cpu->reg[rd] = (i8)imm8;
+            break; // li
+        case 0xE:
+            cpu->running = 0;
+            break; // halt
+        case 0xF:
+            printf("%u\n", cpu->reg[ra]);
+            break; // out
+        default:
+            fprintf(stderr, "Unknown opcode: 0x%X at pc 0x%X\n", opcode, cpu->pc - 1);
+            cpu->running = 0;
+            break;
+        }
+    }
+}
